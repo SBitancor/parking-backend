85d8510bec82aa0f6ec5f2206ef6debbc31ec14b
Samuel Bitancor Jr <bitancor.samueljr@ue.edu.ph>
2022-05-12 21:49:31 +0800

    added controller functions to cashier

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,14 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    
+    public function index()
+    {
+        return Cashier::all();
+    }
+
     public function register(Request $request){
+
         $fields = $request->validate([
             'cashierName'=>'required|string',
             'contactEmail'=>'required|string|unique:cashiers,contactEmail',
@@ -70,5 +77,51 @@ class AuthController extends Controller
         return [
             'message'=>'Logged Out'
         ];
+    }
+
+    public function show($id)
+    {
+        return Cashier::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $fields = $request->validate([
+            'cashierName'=>'required|string',
+            'contactEmail'=>'required|string|unique:cashiers,contactEmail',
+            'contactNumber'=>'required|string|unique:cashiers,contactNumber',
+            'username'=>'required|string|unique:cashiers,username',
+            'password'=>'required|string|confirmed'
+
+        ]);
+
+        $cashier = Cashier::find($id);
+        $cashier->update($request->all());
+
+        if($cashier){
+            return response()->json(['message'=>'Cashier updated successfully'], 200);
+        } else {
+            return response()->json(['message'=>'Cashier update request failed'], 400);
+        }
+        return $cashier;
+    }
+
+    public function search($cashierName)
+    {
+        return Cashier::where('cashierName', 'like', '%'.$cashierName.'%')->get();
+    }
+
+    public function destroy($id)
+    {
+        $query = Cashier::destroy($id);
+
+        if($query){
+            return response()->json(['message'=>'Cashier deleted successfully'], 200);
+        } else {
+            return response()->json(['message'=>'Cashier delete request failed'], 400);
+        }
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,12 @@ use App\Http\Controllers\AuthController;
 Route::get('customers', [CustomerController::class, 'index']);
 Route::get('customers/{id}', [CustomerController::class, 'show']);
 Route::get('customers/search/{name}', [CustomerController::class, 'search']);
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('cashiers/register', [AuthController::class, 'register']);
+Route::post('cashiers/login', [AuthController::class, 'login']);
+Route::get('cashiers', [AuthController::class, 'index']);  //Get Cashier
+
+Route::get('cashiers/{id}', [AuthController::class, 'show']);
+Route::get('cashiers/search/{name}', [AuthController::class, 'search']);
 
 //PROTECTED ROUTES
 Route::group(['middleware'=>['auth:sanctum']], function(){
@@ -20,7 +24,10 @@ Route::group(['middleware'=>['auth:sanctum']], function(){
     Route::put('customers/{id}', [CustomerController::class, 'update']);
     Route::put('customers/checkout/{id}', [CustomerController::class, 'checkout']);
     Route::delete('customers/{id}', [CustomerController::class, 'destroy']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('cashiers/logout', [AuthController::class, 'logout']);
+    Route::delete('cashiers/{id}', [AuthController::class, 'destroy']);    //DElete Cashier
+
+    Route::put('cashiers/{id}', [AuthController::class, 'update']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
